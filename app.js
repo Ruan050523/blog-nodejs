3502742327dcbfeb2e8dc3cfcbf26f9f22270b36
RUAN MIRANDA MARTINS <ruan.mmartins@senacsp.edu.br>
2025-08-13 16:53:21 -0300

    first commit

--- app.js
+++ app.js
@@ -11,12 +11,19 @@
 
  //acessando a rota
  app.get('/', (req, res) => {
-    res.render('index');
- });
+
+//passando parâmetros para o body
+    const blogs = [
+        // {titulo: 'Chamander', conteudo: 'A ponta de seu rabo está constantemente em chamas e se for apagada pode resultar em sua morte.'},
+        // {titulo: 'Squirtle', conteudo: 'Seu casco reduz a resistência contra a água fazendo com que ele nade mais rápido.'},
+        // {titulo: 'Bulbasaur', conteudo: 'Ao evoluir o bulbo começa a desabrochar até se tornar uma grande flor na evolução final.'},
+    ];
+    res.render('index', { titulo: 'Home', blogs});
+});
 
  //nova rota 
  app.get('/sobre', (req, res)  => {
-    res.render('sobre');
+    res.render('sobre', { titulo: 'Sobre'});
  });
  
 
@@ -25,7 +32,12 @@ app.get('/sobrenos', (req, res) => {
     res.redirect('/sobre');
 })
 
+//rota da criação do conteúdo
+app.get('/blog/criar', (req, res) => {
+    res.render('criar', { titulo: 'Criar novo Blog'});
+});
+
 //ERRO 404
 app.use((req, res) => {
-    res.status(404).render('404');
+    res.status(404).render('404', { titulo: '404'});
 });
